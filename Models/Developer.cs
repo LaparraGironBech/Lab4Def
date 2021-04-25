3a82a92448f73ded5c17323e9bddc0050b0e7a4b
Carlos-Laparra <70443027+Carlos-Laparra@users.noreply.github.com>
2021-04-25 00:00:44 -0600

    Cambios Varios

---
+++ Models/Developer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Models
+{
+    public class Developer
+    {
+        string titulo { get; set; }
+        string descripcion { get; set; }
+        string proyecto { get; set; }
+        int prioridad { get; set; }
+        string fecha { get; set; }
+
+        public Developer(string Nombre,string Descripcion,string Proyecto,int Prioridad,string Fecha)
+        {
+            titulo = Nombre;
+            descripcion = Descripcion;
+            proyecto = Proyecto;
+            prioridad = prioridad;
+            fecha = Fecha;
+        }
+    }
+}
---
+++ Models/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Models
+{
+    public class Singleton
+    {
+        private readonly static Singleton _instance = new Singleton();
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+    }
+}
+
