92b9870b1d418346a921e34538058a1e37aaafdc
JasonZamora <JasonZamora@users.noreply.github.com>
2021-04-26 00:32:42 -0600

    Excel

--- Controllers/DevelopersController.cs
+++ Controllers/DevelopersController.cs
@@ -4,12 +4,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Lab4.Models;
 
 namespace Lab4.Controllers
 {
+
+
     public class DevelopersController : Controller
     {
+
+        //Cargar archivo CSV
+        private IHostingEnvironment Environment;
+        public DevelopersController(IHostingEnvironment _environment)
+        {
+            Environment = _environment;
+        }
+        //termina parte de csv
+
         // GET: DeveloperController
         public ActionResult Index()
         {
@@ -98,5 +113,98 @@ namespace Lab4.Controllers
                 return View();
             }
         }
+        //carga csv
+        [HttpPost]
+        public IActionResult Index(IFormFile postedFile)
+        {
+            if (postedFile != null)
+            {
+                string path = Path.Combine(this.Environment.WebRootPath, "Uploads");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                string fileName = Path.GetFileName(postedFile.FileName);
+                string filePath = Path.Combine(path, fileName);
+                using (FileStream stream = new FileStream(filePath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+                string csvData = System.IO.File.ReadAllText(filePath);
+                DataTable dt = new DataTable();
+                bool firstRow = true;
+                foreach (string row in csvData.Split('\n'))
+                {
+                    if (!string.IsNullOrEmpty(row))
+                    {
+                        if (!string.IsNullOrEmpty(row))
+                        {
+                            if (firstRow)
+                            {
+
+                                foreach (string cell in row.Split(','))
+                                {
+
+                                    //dt.Columns.Add(cell.Trim());
+
+                                }
+
+                                firstRow = false;
+                            }
+                            else
+                            {
+                                //dt.Rows.Add();
+                                int i = 0;
+                                int cont = 0;
+                                string[] NodoM = new string[6] { "", "", "", "", "", "" };
+                                int encontrar = 0;
+                                string cell2 = "";
+                                foreach (string cell in row.Split(','))
+                                {
+                                    if (cell.Substring(0, 1) != "\"" && encontrar == 0)
+                                    {
+                                        //dt.Rows[dt.Rows.Count - 1][i] = cell.Trim();
+                                        NodoM[cont] = cell.Trim();
+                                        cell2 = "";
+                                        cont++;
+                                        i++;
+                                    }
+                                    else
+                                    {
+                                        cell2 = cell2 + cell + ", "; encontrar++;
+                                        if (cell.Substring((cell.Length - 1), 1) == "\"")
+                                        {
+                                            encontrar = 0;
+                                            cell2 = cell2.Remove(0, 1);
+                                            cell2 = cell2.Remove(cell2.Length - 3, 3);
+                                            //dt.Rows[dt.Rows.Count - 1][i] = cell2.Trim();
+                                            NodoM[cont] = cell2.Trim();
+                                            cont++;
+                                            i++;
+                                            cell2 = "";
+                                        }
+
+                                    }
+                                }
+//parte para agregar en cada iteración
+                            }
+                        }
+                    }
+                }
+               
+                return RedirectToAction(nameof(Index));
+            }
+
+
+            return View();
+
+        }
+        //termina carga de csv
+
+
+
+
+
     }
 }
