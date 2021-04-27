871505ce37a206512b3473735620133c5efda8ad
martinbech <70785044+martinbechfurlan@users.noreply.github.com>
2021-04-26 21:54:35 -0600

    esqueleto

---
+++ Controllers/ProjectsManualController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab4.Models;
+namespace Lab4.Controllers
+{
+    public class ProjectsManualController : Controller
+    {
+        // GET: ProjectsManualController
+        public ActionResult Index()
+        {
+            return View(Singleton.Instance.ProjectsManualList);
+        }
+
+        // GET: ProjectsManualController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProjectsManualController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProjectsManualController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                var newProjectsManual = new ProjectsManual
+                {
+                    
+                    titulo = collection["titulo"],
+                    descripcion = collection["descripcion"],
+                    proyecto = collection["proyecto"],
+                    prioridad = Convert.ToInt32(collection["prioridad"]),
+                    fecha = collection["proyecto"]
+
+                };
+                Singleton.Instance.ProjectsManualList.Add(newProjectsManual);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProjectsManualController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProjectsManualController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProjectsManualController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProjectsManualController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Models/Singleton.cs
+++ Models/Singleton.cs
@@ -9,12 +9,14 @@ namespace Lab4.Models
     {
         private readonly static Singleton _instance = new Singleton();
         public List<Developers> DevelopersList;
+        public List<ProjectsManual> ProjectsManualList;
         public TablaHash<int, Developer> TablaDePrueba = new TablaHash<int, Developer>();
         //Se crea  la tabla de 10 
             
     private Singleton()
         {
             DevelopersList = new List<Developers>();
+            ProjectsManualList = new List<ProjectsManual>();
 
             //<---------Dar tamaño a la tabla hash------------>
             for (int i = 0; i < 10; i++)
